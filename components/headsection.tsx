74d53964ce5e8537388a3e9e985f83ce73b29fb0
Aldair Ramos <raldair7@gmail.com>
2024-10-16 22:57:40 -0600

    Head Section update

--- components/headsection.tsx
+++ components/headsection.tsx
@@ -7,6 +7,9 @@ const Headsection: React.FC = () => {
       <video autoPlay loop muted playsInline className="absolute w-full h-full object-cover">
         <source src="/assets/herosection.mp4" type="video/mp4" />
       </video>
+      <div className={`flex-grow-0 pt-10 z-10 transition-opacity duration-1000`}>
+        <Image src="/assets/capitalogo.svg" width={128 / 2} height={114 / 2} alt="logo" />
+      </div>
       <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
         <h1 className="mb-6 text-4xl xl:text-5xl">Capital Tech</h1>
         <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
