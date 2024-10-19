bde31d79f0f6460f5a439b5a4c38bea2ce93bc97
Aldair Ramos <raldair7@gmail.com>
2024-10-18 22:57:16 -0600

    About us component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Headsection from "../../components/headsection";
-
+import AboutUs from "../../components/aboutus";
 export default function Home() {
   return (
     <>
 
       <Headsection />
+      <AboutUs />
 
     </>
   );
